58626a3d14fc2e61a282ea75abede6d6d2ea477a
aliyukabir1 <60947300+aliyukabir1@users.noreply.github.com>
2020-11-02 23:17:20 +0100

    Add files via upload

---
+++ My own todo list app/todo.js
@@ -0,0 +1,24 @@
+$("ul").on("click","li", function(){
+$(this).toggleClass("tick")
+})
+
+$("ul").on("click","span", function(event) {
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove()
+       
+    });
+event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+      if(event.which === 13){
+          var text = $(this).val();
+          $(this).val("") ;
+          $("ul").append("<li><span><i class='fa fa-minus' aria-hidden='true'></i></span> "+text+"</li>")
+      }
+
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+})
